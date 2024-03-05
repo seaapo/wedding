22443ad0b1a1e880eb32d76214f428dec3a42459
seaapo <sean.tsai@iadea.com>
2024-03-05 15:11:11 +0800

    - update tab style  - update js control  - slow down the speed of cloud

--- script.js
+++ script.js
@@ -1,42 +1,22 @@
-window.onload = function() {
-    document.getElementById('anchor-intro').addEventListener('click', showIntro);
-    document.getElementById('anchor-photo').addEventListener('click', showPhoto);
-    document.getElementById('anchor-ig').addEventListener('click', showIg);
-    document.getElementById('anchor-map').addEventListener('click', showMap);
-}
+let _controls = [
+    { id: 'intro' },
+    { id: 'photo' },
+    { id: 'map' }
+]
 
-function hideAllContent() {
-    let ids = ['intro', 'photo', 'ig', 'map'];
-    for (let id of ids) {
-        document.getElementById('anchor-' + id).classList.remove('active');
-        document.getElementById('card-' + id).classList.add('d-none');
+window.onload = function () {
+    for (let control of _controls) {
+        document.getElementById('anchor-' + control.id).addEventListener('click', function (e) {
+            hideAllContent();
+            document.getElementById('anchor-' + control.id).classList.add('active');
+            document.getElementById('card-' + control.id).classList.remove('d-none');
+        });
     }
 }
 
-function showIntro() {
-    hideAllContent();
-
-    document.getElementById('anchor-intro').classList.add('active');
-    document.getElementById('card-intro').classList.remove('d-none');
-}
-
-function showPhoto() {
-    hideAllContent();
-
-    document.getElementById('anchor-photo').classList.add('active');
-    document.getElementById('card-photo').classList.remove('d-none');
-}
-
-function showIg() {
-    hideAllContent();
-
-    document.getElementById('anchor-ig').classList.add('active');
-    document.getElementById('card-ig').classList.remove('d-none');
-}
-
-function showMap() {
-    hideAllContent();
-
-    document.getElementById('anchor-map').classList.add('active');
-    document.getElementById('card-map').classList.remove('d-none');
+function hideAllContent() {
+    for (let control of _controls) {
+        document.getElementById('anchor-' + control.id).classList.remove('active');
+        document.getElementById('card-' + control.id).classList.add('d-none');
+    }
 }
