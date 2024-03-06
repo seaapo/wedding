197e020d8afee1547f2a608dfbd05ccf0a93927a
seaapo <sean.tsai@iadea.com>
2024-03-06 11:02:35 +0800

    - Add photo gallery

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let _controls = [
     { id: 'intro' },
     { id: 'photo' },
     { id: 'map' }
-]
+];
 
 window.onload = function () {
     for (let control of _controls) {
@@ -11,6 +11,20 @@ window.onload = function () {
             document.getElementById('anchor-' + control.id).classList.add('active');
             document.getElementById('card-' + control.id).classList.remove('d-none');
         });
+    }
+
+    const imgPlaceHolder = document.getElementById('photo-placeholder');
+    if (imgPlaceHolder) {
+        for (let i = 1; i < 6; i++) {
+            const img = document.createElement("img");
+            img.src = 'resources/pic' + i + '.jpg';
+            img.classList.add('photo');
+            const gallery = document.getElementById('photo-gallery');
+            gallery.appendChild(img);
+            img.addEventListener('click', function () {
+                imgPlaceHolder.style['background-image'] = "url(" + img.src + ")";
+            });
+        }
     }
 }
 
